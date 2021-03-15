26376010527bfbf880b666f933c0f752ce2f04e3
yjnoh1016 <yjnoh1016@naver.com>
2021-03-15 20:44:51 +0900

    210315 first commit

--- My Album/ViewController.swift
+++ My Album/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "My First App!!!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello", message: "My First App!!", preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
