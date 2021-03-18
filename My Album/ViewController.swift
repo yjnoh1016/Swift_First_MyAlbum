37ea83d12f670a0360bbdf7d58ff97d6b54a66bc
yjnoh1016 <yjnoh1016@naver.com>
2021-03-18 11:22:27 +0900

    210318 [UPDATE] ViewController

--- My Album/ViewController.swift
+++ My Album/ViewController.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "My First App!!", preferredStyle: .alert)
+        let message = "가격은 ₩\(currentValue) 입니다"
+        
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
     }
     
 }
